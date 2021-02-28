bdcd7908d69ec3d03b7b9dd032952cf4f3a2b88f
BuHogeJI2 <dmitrydemidovich@yandex.ru>
2021-02-28 19:49:00 +0300

    Create functionality for deleting tags

--- component/TagsComponent.js
+++ component/TagsComponent.js
@@ -2,34 +2,55 @@ class TagsComponent {
 
     constructor() {
         this.tags = [];
-        this.showingTags = [];
         this.crossImgUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flat_cross_icon.svg/1200px-Flat_cross_icon.svg.png'
     }
 
-    showTags(block) {
-        if (this.tags.length !== this.showingTags.length) {
-            const newTags = this.tags.slice(this.showingTags.length);
-            newTags.forEach(tag => {
-                block.innerHTML += `<div class="tag">${tag} <img src="${this.crossImgUrl}" alt=""></div>`
-                this.showingTags.push(tag);
-            })
-        }
+    get getTags() {
+        return this.tags;
     }
 
-    handleAddClick() {
+    set setTags(value) {
+        this.tags = value;
+    }
+
+    addTag(tag) {
+        this.tags.push(tag);
+    }
+
+    deleteTag(removingTag) {
+        this.tags = this.tags.filter(tag => tag !== removingTag);
+    }
+
+    renderTags() {
+
+        const block = document.querySelector('.tags_block');
+        const tagDiv = (tag) => `<div class="tag">${tag} <img name="${tag}" class="cross_img" src="${this.crossImgUrl}"></div>`
+
+        block.innerHTML = '';
+        this.tags.forEach(tag => block.innerHTML += tagDiv(tag));
+    }
+
+    handleAddBtnClick() {
         const input = document.querySelector('.input');
-        const tagsBlock = document.querySelector('.tags_block');
         const inputTags = input.value.split(' ');
 
-        inputTags.forEach(tag => tag && this.tags.push(tag));
-        this.showTags(tagsBlock);
+        inputTags.forEach(tag => tag && this.addTag(tag));
+        this.renderTags();
         input.value = '';
+    }
 
+    handleDeleteBtnClick(event) {
+        if (event.target.className === 'cross_img') {
+            const tag = event.target.name;
+            this.deleteTag(tag);
+            this.renderTags();
+        }
     }
 
     listener() {
         const btn = document.querySelector('.component_btn');
-        btn.addEventListener('click', this.handleAddClick.bind(this));
+        btn.addEventListener('click', this.handleAddBtnClick.bind(this));
+        document.addEventListener('click', this.handleDeleteBtnClick.bind(this));
     }
 
     renderInput() {
